57fc29d7faab5d59197fe7e182f0b0aeb9b6cc7f
yosefdebela <yosef.debela@ju.edu.et>
2026-01-22 17:01:23 +0300

    edited the view

--- account/views.py
+++ account/views.py
@@ -11,24 +11,24 @@ def  home(request):
     return render(request, 'account/home.html')
 
 
-def user_login(request):
-    if request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
-            user = authenticate(request, username = cd['username'], password = cd['password'])
+# def user_login(request):
+#     if request.method == 'POST':
+#         form = LoginForm(request.POST)
+#         if form.is_valid():
+#             cd = form.cleaned_data
+#             user = authenticate(request, username = cd['username'], password = cd['password'])
 
-            if user is not None:
-                if user.is_active:
-                    login(request, user)
-                    return HttpResponse('Authenticated successfully')
-                else:
-                    return HttpResponse('disabled account')
-            else:
-                return HttpResponse('invalid login')
-    else:
-        form = LoginForm()
-    return render(request, 'account/login.html', {'form': form})
+#             if user is not None:
+#                 if user.is_active:
+#                     login(request, user)
+#                     return HttpResponse('Authenticated successfully')
+#                 else:
+#                     return HttpResponse('disabled account')
+#             else:
+#                 return HttpResponse('invalid login')
+#     else:
+#         form = LoginForm()
+#     return render(request, 'account/login.html', {'form': form})
 
 @login_required
 def dashboard(request):
